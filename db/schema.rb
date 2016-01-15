445d219e31c7b2ec1a22dcdb80ea9992de7dcc31
FiXerCz <fixercz@gmail.com>
2016-01-15 23:31:58 +0100

    Configured cancancan (Ability model), added 'admin' attribute to User and minor band views tweaks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115181359) do
+ActiveRecord::Schema.define(version: 20160115204144) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160115181359) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
