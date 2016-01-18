b14a7ee5f3eeaa45d02e03b3293c9d5a614e1c01
FiXerCz <fixercz@gmail.com>
2016-01-18 12:11:05 +0100

    Fixed not working home page for nonauthenticated user.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @user = User.where(:id => current_user.id)[0]
+    @user = User.where(:id => current_user.id)[0] if user_signed_in?
   end
 end
