245cb87ee48638e306439aea664f35e8fe9cf456
FiXerCz <fixercz@gmail.com>
2016-01-20 19:27:46 +0100

    Band member validation temp workaround.

--- app/models/band.rb
+++ app/models/band.rb
@@ -22,7 +22,7 @@ class Band < ActiveRecord::Base
   validate :has_from_one_to_five_genres
 
   def has_at_lest_one_member
-    errors.add(:role, 'Must have at least 1 member') if self.members.blank?
+    #errors.add(:role, 'Must have at least 1 member') if self.members.blank?
   end
 
   def has_from_one_to_five_genres
