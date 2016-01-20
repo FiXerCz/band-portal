324f3f56fa2239189cdfe46022f1b53efda43450
Petr Barton <petrbarton2@gmail.com>
2016-01-20 22:21:12 +0100

    initial albums and songs

---
+++ app/views/albums/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @album, :id, :title, :released, :band_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :songs
+  resources :albums
+
   devise_for :users
 
   resources :users, only: [:show,:index]
