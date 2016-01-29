a168d56520b61cdcca67643ea74af7d17b4d9faa
Petr Barton <petrbarton2@gmail.com>
2016-01-29 17:23:14 +0100

    add email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :domain         => 'herokuapp.com/',
+    :user_name      => ENV["GMAIL_USERNAME"],
+    :password       => ENV["GMAIL_PASSWORD"],
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => "https://infinite-garden-5539.herokuapp.com" }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
