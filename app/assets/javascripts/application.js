d7d9abc5ac02d809d3be75a261bc080f840c03ee
FiXerCz <fixercz@gmail.com>
2016-02-01 19:09:58 +0100

    Replaced js code to style select element, changed style for song list in album form.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,12 @@
 //= require cocoon
 
 $(document).ready(function() {
+    $('.selectpicker').selectpicker({
+        size: 4
+    });
+});
+
+$(document).ready(function() {
     if (location.hash !== '') $('a[href="' + location.hash + '"]').tab('show');
     return $('a[data-toggle="tab"]').on('shown', function(e) {
         return location.hash = $(e.target).attr('href').substr(1);
