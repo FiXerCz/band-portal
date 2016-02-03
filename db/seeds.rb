ff2a192517259dc358df2b46894a72ae84c02f86
FiXerCz <fixercz@gmail.com>
2016-02-03 20:29:10 +0100

    Added crapload of randomly generated test users to be later used in some dummy bands; all users have password 'password'.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,25 @@
 end
 
 # Portal admin
-User.create!(:email => 'admin@admin.com', :password => 'password', :password_confirmation => 'password',
+User.create(:email => 'admin@admin.com', :password => 'password', :password_confirmation => 'password',
              :confirmation_token => 'umU7TefX37dqFE4MpMgn', :confirmed_at => '2016-01-01 00:00:00',
              :confirmation_sent_at => '2016-01-01 00:00:00', :unconfirmed_email => nil, :admin => true,
              :username => 'admin')
+
+# Some randomly generated test users
+['Steven Simons', 'Sylvia Stacy', 'Jules Fabron', 'Patrina Murton', 'Hank Janas', 'Cammie Plasencia', 'Mitsuko Voshell',
+ 'Charlott Beamon', 'Clemente Mary', 'Roberto Troiano', 'Jon Villano', 'Wade Lamoureaux', 'Myesha Sinegal',
+ 'Wyatt Mercedes', 'Arie Prichard', 'Vita Derosa', 'Jacquelin Locicero', 'Tisa Benner', 'Mauro Marotta', 'Claud Heger',
+ 'Vernita English', 'Justine Eich', 'Trevor Newman', 'Moses Newton', 'Wayne Brown', 'Micheal Casey', 'Lucas Mitchell',
+ 'Michelle Mckinney', 'Meghan Parker', 'Gail Duncan', 'Jeannette Sutton', 'Sheri Francis', 'Harold Cannon',
+ 'Floyd Kim'].each do |name|
+  firstname = name.downcase.split(' ')[0]
+  surname = name.downcase.split(' ')[1]
+  email = firstname + '.' + surname +'@fakemail.tld'
+  username = firstname[0] + surname
+
+  User.create(:email => email, :password => 'password', :password_confirmation => 'password',
+               :confirmation_token => 'umU7TefX37dqFE4MpMgn', :confirmed_at => '2016-01-01 00:00:00',
+               :confirmation_sent_at => '2016-01-01 00:00:00', :unconfirmed_email => nil, :admin => false,
+               :username => username, :fullname => name)
+end
